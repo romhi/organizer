1e45b21c03f0a0aaecf55521ba3366648b4233be
Roman Khitroff <roman.khitroff@gmail.com>
2016-06-02 15:24:35 +0300

    add production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  Rails.application.routes.default_url_options[:host] = 'romhi.ru'
+  config.action_mailer.default_url_options = { :host => 'romhi.ru' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = YAML.load(File.open("#{Rails.root}/config/smtp.yml"))
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
