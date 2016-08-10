a631f74461941aa71d4a41b3f94ff8fef3c933e8
Roman Khitroff <roman.khitroff@gmail.com>
2016-08-10 13:36:33 +0300

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :first_name, :last_name, :phone, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :phone, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :current_password])
   end
 
   def select_by_role
